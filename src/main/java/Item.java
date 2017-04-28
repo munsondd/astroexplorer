a35df21b95e923fcf90a8707d45d21c772dfa1b7
D-Kleck <david.kleckner@gmail.com>
2017-04-28 13:07:47 -0600

    added some functions to Item

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -1,6 +1,29 @@
 class Item {
-	private int type;
-	private int sprite;
+	private int maxItems = 10;
+        Item[] itemList = new Item[maxItems];
+
+	private int id;
+	String name;
+	int price;
 	int quantity;
 
-}
+	Item(){
+    	id = 0;
+  	}
+  	private Item(String name,int price,int quantity) {
+	    this.name = name;
+	    this.price = price;
+	    this.quantity = quantity;
+	}
+	void FindItem(int id){
+      System.out.println(itemList[id].id + " " + itemList[id].name + " " +
+                    itemList[id].price + " " + itemList[id].quantity);
+        }
+    void displayItemList(){
+        System.out.println("ID " + "Name " + "Price " + "Quantity ");
+        for (int i = 0; i < id; i++){
+            System.out.println(itemDB[i].id + " " + itemDB[i].name + " " +
+                    itemDB[i].price + " " + itemDB[i].quantity);
+        }
+    }
+  }
