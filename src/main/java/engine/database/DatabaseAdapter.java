473e8869f8d32f0c7601e89dd7f5fc71d86758e1
Evan Su <evan.su@colorado.edu>
2017-04-28 13:47:38 -0600

    Add SQLite operations to database adapter.

--- src/main/java/engine/database/DatabaseAdapter.java
+++ src/main/java/engine/database/DatabaseAdapter.java
@@ -1,11 +1,25 @@
 package engine.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DatabaseAdapter {
 
     private String path;
+    private String jdbc;
 
     public DatabaseAdapter(String filename) {
         this.path = filename;
+        this.jdbc = "jdbc:sqlite:" + filename;
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException e) {
+            // could not find a JDBC adapter
+            // warn user that sessions will not be saved
+        }
     }
 
     public void save() {
@@ -14,6 +28,16 @@ public class DatabaseAdapter {
 
     public void load() {
 
+    }
+
+    private Connection getConnection() {
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(this.jdbc);
+        } catch(SQLException e) {
+
+        }
     }
 
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -13,8 +13,8 @@ public class AppTest {
 
     @Test public void testLocationDistance() {
         World world = new World();
-        Location l1 = new Location(1.0,3.0, world);
-        Location l2 = new Location(6.0,9.0, world);
+        Location l1 = new Location(1.0, 3.0, world);
+        Location l2 = new Location(6.0, 9.0, world);
         assertEquals(7.81025, l1.distance(l2),0.00005);
     }
 
