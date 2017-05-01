adc6721a71fabbef790dc031f0b088cf07e3743e
Evan Su <evan.su@colorado.edu>
2017-04-30 18:33:33 -0600

    Add constants file, rewrite main game loop.

--- src/main/java/AstroExplorerText.java
+++ src/main/java/AstroExplorerText.java
@@ -1,18 +1,59 @@
+import engine.text.Constants;
+
 import java.util.Scanner;
 
-/**
- * Created by david on 4/30/17.
- */
 public class AstroExplorerText {
-    public static void main(String args[]) {
 
+    private boolean running = true;
+    private Scanner in = new Scanner(System.in);
+
+    /**
+     * Starts the game loop. This will block until the
+     * game exits.
+     */
+    public void start() {
+        System.out.println(Constants.WELCOME);
+
+        while (this.running) {
+            String in = this.prompt();
+            this.dispatch(in);
+        }
+    }
+
+    /**
+     * Exit the game. This will only run when invoked within
+     * the game loop, because the main thread will be blocked.
+     */
+    public void exit() {
+        this.running = false;
+    }
+
+    /**
+     * Blocks and prompts for a response from the player.
+     * @return String the player's response
+     */
+    public String prompt() {
+        return this.in.next();
+    }
+
+    /**
+     * Handles a player response.
+     * @param input The command to be run
+     */
+    public void dispatch(String input) {
+        System.out.println("DISPATCH: " + input);
+    }
+
+    public static void main(String args[]) {
+        AstroExplorerText game = new AstroExplorerText();
+        game.start();
+
+        /*
         boolean inGame = true;
         boolean alive = true;
         Scanner reader = new Scanner(System.in);
         String input;
 
-
-        //the main game loop
         while(inGame && alive){
             System.out.println("Welcome brave astroexplorer. Your ship has broken down in the middle of an asteroid field." +
                     "You must gather surrounding resources to fix it. but be careful you only have a limited supply of fuel and" +
@@ -22,6 +63,7 @@ public class AstroExplorerText {
 
 
         }
+        */
     }
 
 }
---
+++ src/main/java/engine/text/Constants.java
@@ -0,0 +1,9 @@
+package engine.text;
+
+public class Constants {
+
+    public static final String WELCOME = "Welcome brave astroexplorer. Your ship has broken down in the middle of an" +
+            "asteroid field. You must gather resources from your surroundings to fix it. Be careful though! You only" +
+            "have a limited supply of fuel and oxygen. You will need to return to the ship to resupply.";
+
+}
