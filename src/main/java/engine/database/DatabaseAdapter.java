a0bc3d90422156317cf13bedc94aa599ad623def
Evan Su <evan.su@colorado.edu>
2017-05-01 23:19:57 -0600

    Increase verbosity on SQLite database operations.

--- src/main/java/engine/database/DatabaseAdapter.java
+++ src/main/java/engine/database/DatabaseAdapter.java
@@ -16,32 +16,38 @@ public class DatabaseAdapter {
         } catch (ClassNotFoundException e) {
             // could not find a JDBC adapter
             // warn user that sessions will not be saved
+            System.out.print("WARNING: SQLite driver failed to load. Game state will not be saved.");
+            e.printStackTrace();
         }
     }
 
     public boolean set(KeySpecifier key, String value) {
         try {
-            Connection connection = this.getConnection();
-            Statement statement = connection.createStatement();
+            Statement statement = this.createStatement();
             statement.executeUpdate("CREATE TABLE IF NOT EXISTS jsonmap (id integer, json string)");
             statement.executeUpdate("INSERT INTO jsonmap values(" + key.ordinal() + ", '" + value + "')");
             return true;
         } catch(SQLException e) {
+            e.printStackTrace();
             return false;
         }
     }
 
     public String get(KeySpecifier key) {
         try {
-            Connection connection = this.getConnection();
-            Statement statement = connection.createStatement();
+            Statement statement = this.createStatement();
             ResultSet rs = statement.executeQuery("SELECT json FROM jsonmap WHERE id=" + key.ordinal());
             // we only care about the first entry, because they are unique
             rs.next();
             return rs.getString("json");
         } catch(SQLException e) {
+            e.printStackTrace();
             return null;
         }
+    }
+
+    private Statement createStatement() throws SQLException {
+        return this.getConnection().createStatement();
     }
 
     private Connection getConnection() throws SQLException {
