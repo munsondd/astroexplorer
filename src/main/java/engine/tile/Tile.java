7f7190c05c793720125f9fea5add639632496a28
David <munsondd@colorado.edu>
2017-05-02 09:52:16 -0600

    fixed toString in Tile

--- src/main/java/engine/tile/Tile.java
+++ src/main/java/engine/tile/Tile.java
@@ -83,10 +83,10 @@ public class Tile implements ITile {
 	public String toString() {
 		String str;
 		if (this.type == 1) str = "space tile";
-		if (this.type == 2) str = "asteroid tile";
-		if (this.type == 3) str = "resource tile";
-		if (this.type == 4) str = "ship tile";
-		if (this.type == 5) str = "shop tile";
+		else if (this.type == 2) str = "asteroid tile";
+		else if (this.type == 3) str = "resource tile";
+		else if (this.type == 4) str = "ship tile";
+		else if (this.type == 5) str = "shop tile";
         else str = "blank tile";
 		return str;
 	}
